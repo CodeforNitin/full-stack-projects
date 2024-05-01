631c0947900b35f2f0057c397d7e59a53bf55461
CodeforNitin <codeforni3@gmail.com>
2024-05-01 18:57:28 +0530

    defining schema for workout

---
+++ p1-workout-buddy/backend/models/workoutModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema  = mongoose.Schema;
+
+//defining schema
+const workoutSchema = new Schema({
+    title : {
+        type: String,
+        required: true,
+    },
+
+    reps:{
+        type: Number,
+        required: true
+    },
+
+    weight:{
+        type: Number,
+        required: true
+    }
+}, { timestamps: true })
+
+//model name = Workout (always make this singular beacsue anyhow it gonna convert it into Workouts)
+module.exports = mongoose.model('Workout', workoutSchema);
