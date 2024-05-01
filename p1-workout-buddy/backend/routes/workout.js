745eceaf16bdf8daf6f30b4196f4ae61b42226d6
CodeforNitin <codeforni3@gmail.com>
2024-05-01 17:59:03 +0530

    basic routes temlate created for all type pf requests

--- p1-workout-buddy/backend/routes/workout.js
+++ p1-workout-buddy/backend/routes/workout.js
@@ -2,6 +2,32 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', () => {})
+//GET all workouts
+router.get('/', (req,res) => {
+    res.json({msg : 'GET all workouts'})
+})
+
+//GET single workouts
+router.get('/:id', (req,res) => {
+    res.json({msg : 'GET single workouts'})
+})
+
+//POST single workouts
+router.post('/', (req,res) => {
+    res.json({msg : 'POST workouts'})
+})
+
+
+//DELETE single workouts
+router.delete('/:id', (req,res) => {
+    res.json({msg : 'DELETE workouts'})
+})
+
+
+//UPDATE single workouts
+router.patch('/:id', (req,res) => {
+    res.json({msg : 'UPDATE workouts'})
+})
+
 
 module.exports = router;
