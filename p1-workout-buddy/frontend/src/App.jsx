e3906aeadc07ddddaec60b4dc859528f8f48f4c2
CodeforNitin <codeforni3@gmail.com>
2024-05-03 23:47:07 +0530

    removing clutter and setting up skeleton for our site

--- p1-workout-buddy/frontend/src/App.jsx
+++ p1-workout-buddy/frontend/src/App.jsx
@@ -1,7 +1,25 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+
 function App() {
   return (
     <>
-    <h1>Heelo world</h1>
+    <div className='App'>
+      <BrowserRouter>
+      <Navbar />
+        <div className='pages'>
+          <Routes>
+            <Route
+              path="/"
+              element={<Home/>} 
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+
+    </div>
     </>
   )
 }
