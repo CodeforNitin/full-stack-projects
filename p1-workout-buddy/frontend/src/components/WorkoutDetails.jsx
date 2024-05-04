dae140d96d78772c8ade3ce1752651c8dd0210a0
CodeforNitin <codeforni3@gmail.com>
2024-05-05 00:42:12 +0530

    delete functionality

--- p1-workout-buddy/frontend/src/components/WorkoutDetails.jsx
+++ p1-workout-buddy/frontend/src/components/WorkoutDetails.jsx
@@ -1,10 +1,28 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
 const WorkoutDetails = ({workout}) => {
+
+    const { dispatch } = useWorkoutsContext();
+
+    const handleDeleteClick = async () => {
+        const repsonse = await fetch('/api/workouts/' + workout._id, {
+            method : 'DELETE'
+        })
+
+        const json = await repsonse.json()
+
+        if (repsonse.ok){
+            dispatch({type: 'DELETE_WORKOUT', payload: json})
+        }
+
+    }
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
             <p><strong>Reps: </strong>{workout.reps}</p>
             <p><strong>Weight: </strong>{workout.weight}</p>
             <p>{workout.createdAt}</p>
+            <span onClick={handleDeleteClick}>delete </span>
         </div>
     )
 }
