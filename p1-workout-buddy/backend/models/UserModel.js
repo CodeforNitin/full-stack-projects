f5a68a6f9692510ae64fe069a882eb91e18a6ed3
CodeforNitin <codeforni3@gmail.com>
2024-05-06 01:11:49 +0530

    added bcrypted password feature - hash passwords

--- p1-workout-buddy/backend/models/UserModel.js
+++ p1-workout-buddy/backend/models/UserModel.js
@@ -1,41 +1,30 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
-const { isEmail } = require('validator')
-
-
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema ({
-
-    email : {
-        type: String,
-        reuired: [true, 'Please enter an email'],
-        unique: true,
-        lowercase: true,
-        validate: [isEmail, 'Please enter a valid email address']
-    },
-
-    password : {
-        type: String,
-        reuired: [true, 'Please enter a Password'],
-        minlength: [6, 'Minimum password length is 6 charactres']
-    }
-}, { timestamps: true })
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Please enter an email'],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
+  }
+});
 
 
-//fire funtion after event ocuur
-userSchema.post('save', function(doc, next){
-    console.log('new user is created and saved')
-    next();
-})
+// fire a function before doc saved to db
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
-//fire funtion before event ocuur
-userSchema.post('save', function(next){
-    console.log('new user is about to be created', this)
-    next();
-})
-
-
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
