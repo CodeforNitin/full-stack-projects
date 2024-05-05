107d36a2ae598df0d239999dc410fe686a4051df
CodeforNitin <codeforni3@gmail.com>
2024-05-06 00:47:37 +0530

    custom error messages for validation failled in DB

--- p1-workout-buddy/backend/models/UserModel.js
+++ p1-workout-buddy/backend/models/UserModel.js
@@ -1,19 +1,24 @@
 const mongoose = require('mongoose');
+
+const { isEmail } = require('validator')
+
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema ({
 
     email : {
         type: String,
-        reuired: true,
+        reuired: [true, 'Please enter an email'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email address']
     },
 
     password : {
         type: String,
-        reuired: true,
-        minlength: 6
+        reuired: [true, 'Please enter a Password'],
+        minlength: [6, 'Minimum password length is 6 charactres']
     }
 }, { timestamps: true })
 
