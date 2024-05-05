7cd94192f05632240ed456671109ad2617a0e702
CodeforNitin <codeforni3@gmail.com>
2024-05-06 00:24:05 +0530

    updated POST request -- new user signup

--- p1-workout-buddy/backend/controllers/authController.js
+++ p1-workout-buddy/backend/controllers/authController.js
@@ -1,10 +1,22 @@
+const mongoose = require('mongoose');
+const User = require('../models/UserModel')
+
 
 const signup_get = async (req, res) => {
     res.send('signup')
 }
 
 const signup_post = async (req, res) => {
-    res.send(' new signup')
+
+    const {email, password} = req.body;
+    try{
+        const newUser = await User.create({email, password})
+        res.status(201).json(newUser)
+    }
+    catch (err){
+        console.log(err)
+        res.status(400).json('error: user not created')
+    }
 }
 
 const login_get = async (req, res) => {
