82976227c9db49ab5b17115fd45d69e718c39383
CodeforNitin <codeforni3@gmail.com>
2024-05-08 16:26:41 +0530

    created AuthContext

--- p1-workout-buddy/frontend/src/main.jsx
+++ p1-workout-buddy/frontend/src/main.jsx
@@ -4,11 +4,14 @@ import App from './App.jsx'
 import './index.css'
 
 import { WorkoutsContextProvider } from './context/WorkoutsContext.jsx'
+import { AuthContextProvider } from './context/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthContextProvider>
     <WorkoutsContextProvider>
       <App />
     </WorkoutsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
 )
