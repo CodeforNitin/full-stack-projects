5055ba9816d69acdf67a777eab3c9db7eb9c9b9e
CodeforNitin <codeforni3@gmail.com>
2024-05-08 16:39:05 +0530

    added Routes to login and signup

--- p1-workout-buddy/frontend/src/App.jsx
+++ p1-workout-buddy/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import Home from './pages/Home'
 import Signup from './pages/Signup'
+import Login from './pages/Login'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -20,10 +21,10 @@ function App() {
               path="/signup"
               element={<Signup/>} 
             />
-            {/* <Route
+            <Route
               path="/login"
               element={<Login/>} 
-            /> */}
+            />
           </Routes>
         </div>
       </BrowserRouter>
