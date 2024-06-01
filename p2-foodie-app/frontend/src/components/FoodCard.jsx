2d47e4e4647333338adfa0fe5b371430d7631914
CodeforNitin <codeforni3@gmail.com>
2024-06-01 17:30:58 +0530

    adjusted dynamic foodItems

--- p2-foodie-app/frontend/src/components/FoodCard.jsx
+++ p2-foodie-app/frontend/src/components/FoodCard.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import {FaStar} from "react-icons/fa"
 
-const FoodCard = () => {
+const FoodCard = ({id, name, price, desc, rating, img}) => {
   return (
     <div className='w-[250px] p-5 bg-white font-bold flex flex-col rounded-lg'>
-        <img src="https://img.freepik.com/free-photo/seafood-pizza_74190-5944.jpg?w=996&t=st=1693062328~exp=1693062928~hmac=53fd9ad496580db41c6ca8066510cd89c6b0a0389de8bb6b875a78a1eda09cb5
-         " alt=""
+        <img src={img} alt=""
          className='w-auto h-[130px] hover:scale-110 cursor-grab transition-all duration-500 ease-in-out mb-5'></img>
          <div className='text-sm flex justify-between'>
-            <h2>Onion Pizza</h2>
-            <span className='text-green-500'>₹180</span>
+            <h2>{name}</h2>
+            <span className='text-green-500'>₹{price}</span>
          </div>
-         <p className='text-sm font-normal'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, voluptas?</p>
+         <p className='text-sm font-normal'>{desc.slice(0,50)}...</p>
          <div className='flex justify-between'>
-            <span className='flex justify-center items-center'><FaStar className='mr-1 text-yellow-400'/> 4.5</span>
+            <span className='flex justify-center items-center'><FaStar className='mr-1 text-yellow-400'/> {rating}</span>
             <button className='p-1 text-white bg-green-500 hover:bg-green-600 rounded-lg text-sm'>Add to Cart</button>
          </div>
     </div>
--- p2-foodie-app/frontend/src/components/FoodItem.jsx
+++ p2-foodie-app/frontend/src/components/FoodItem.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import FoodCard from './FoodCard';
+import FoodData from './../data/FoodData';
 
 const FoodItem = () => {
   return (
-    <div><FoodCard/></div>
+    <div className='flex flex-wrap lg:justify-evenly mx-[6px] my-10'>
+        {
+            FoodData.map((food)=>{
+                return <FoodCard 
+                    key={food.id} 
+                    id={food.id} 
+                    name={food.name}
+                    price={food.price}
+                    desc={food.desc}
+                    rating={food.rating}
+                    img={food.img}    
+                />
+               })
+        }
+    </div>
   )
 }
 
