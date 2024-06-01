8da3c958cb439c275669ca55080e95c300b26b49
CodeforNitin <codeforni3@gmail.com>
2024-06-01 18:12:48 +0530

    Added ItemCard Compoennt in Cart

--- p2-foodie-app/frontend/src/components/Cart.jsx
+++ p2-foodie-app/frontend/src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { MdClose } from "react-icons/md";
+import ItemCard from './ItemCard';
 
 
 
@@ -11,6 +12,29 @@ const Cart = () => {
             <span className='text-xl font-bold text-gray-800'>My Order</span>
             <MdClose className='border-2 border-gray-600 font-bold p-1 text-xl rounded-md hover:text-red-500 hover:border-red-300 cursor-pointer'/>
         </div>
+
+        {/* <ItemCard/>{cartItems.length > 0 ? (
+          cartItems.map((food) => {
+            return (
+              <ItemCard
+                key={food.id}
+                id={food.id}
+                name={food.name}
+                price={food.price}
+                img={food.img}
+                qty={food.qty}
+              />
+            );
+          })
+        ) : (
+          <h2 className="text-center text-xl font-bold text-gray-800">
+            Your cart is empty
+          </h2>
+        )} */}
+
+            <ItemCard/>
+            <ItemCard/>
+            <ItemCard/>
 
         <div className='absolute bottom-0'>
             <h3 className='font-semibold text-gray-800'>Items: </h3>
